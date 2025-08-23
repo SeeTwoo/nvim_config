77b25a3772bb39ff5c3a078f7da195aa1272bdf5
SeeTwoo <walti.bee@gmail.com>
2025-08-23 20:41:48 +0200

    changed lualine to more demureness

--- lua/blue_pink_lualine_theme.lua
+++ lua/blue_pink_lualine_theme.lua
@@ -1,39 +1,39 @@
 local M = {}
 
 M.normal = {
-  a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-  b = { fg = "Black", bg = "White" },
-  c = { fg = "Black", bg = "Thistle" },
+	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
+	b = { fg = "Black", bg = "Grey" },
+	c = { fg = "Black", bg = "DarkGrey" },
 }
 
 M.insert = {
-  a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-  b = { fg = "Black", bg = "White" },
-  c = { fg = "Black", bg = "Thistle" },
+	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
+	b = { fg = "Black", bg = "Grey" },
+	c = { fg = "Black", bg = "DarkGrey" },
 }
 
 M.visual = {
-  a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-  b = { fg = "Black", bg = "White" },
-  c = { fg = "Black", bg = "Thistle" },
+	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
+	b = { fg = "Black", bg = "Grey" },
+	c = { fg = "Black", bg = "DarkGrey" },
 }
 
 M.replace = {
-  a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-  b = { fg = "Black", bg = "White" },
-  c = { fg = "Black", bg = "Thistle" },
+	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
+	b = { fg = "Black", bg = "Grey" },
+	c = { fg = "Black", bg = "DarkGrey" },
 }
 
 M.command = {
-  a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-  b = { fg = "Black", bg = "White" },
-  c = { fg = "Black", bg = "Thistle" },
+	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
+	b = { fg = "Black", bg = "Grey" },
+	c = { fg = "Black", bg = "DarkGrey" },
 }
 
 M.inactive = {
-  a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-  b = { fg = "Black", bg = "White" },
-  c = { fg = "Black", bg = "Thistle" },
+	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
+	b = { fg = "Black", bg = "Grey" },
+	c = { fg = "Black", bg = "DarkGrey" },
 }
 
 return M
