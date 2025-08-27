ae6206e459f65f3dec827b7a9f8f885e6ea10542
SeeTwoo <walti.bee@gmail.com>
2025-08-27 15:59:36 +0200

    changed lualine theme for more *light* yaknow

--- lua/blue_pink_lualine_theme.lua
+++ lua/blue_pink_lualine_theme.lua
@@ -1,39 +1,51 @@
 local M = {}
 
 M.normal = {
-	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-	b = { fg = "Black", bg = "Grey" },
-	c = { fg = "Black", bg = "DarkGrey" },
+	a = { fg = "Black", bg = "Cyan", gui = "bold" },
+	b = { fg = "Black", bg = "LightBlue" },
+	c = { fg = "Black", bg = "LightGrey" },
+	y = { fg = "Black", bg = "Pink" },
+	z = { fg = "White", bg = "DeepPink", gui = "bold" },
 }
 
 M.insert = {
-	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-	b = { fg = "Black", bg = "Grey" },
-	c = { fg = "Black", bg = "DarkGrey" },
+	a = { fg = "Black", bg = "Cyan", gui = "bold" },
+	b = { fg = "Black", bg = "LightBlue" },
+	c = { fg = "Black", bg = "LightGrey" },
+	y = { fg = "Black", bg = "Pink" },
+	z = { fg = "White", bg = "DeepPink", gui = "bold" },
 }
 
 M.visual = {
-	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-	b = { fg = "Black", bg = "Grey" },
-	c = { fg = "Black", bg = "DarkGrey" },
+	a = { fg = "Black", bg = "Cyan", gui = "bold" },
+	b = { fg = "Black", bg = "LightBlue" },
+	c = { fg = "Black", bg = "LightGrey" },
+	y = { fg = "Black", bg = "Pink" },
+	z = { fg = "White", bg = "DeepPink", gui = "bold" },
 }
 
 M.replace = {
-	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-	b = { fg = "Black", bg = "Grey" },
-	c = { fg = "Black", bg = "DarkGrey" },
+	a = { fg = "Black", bg = "Cyan", gui = "bold" },
+	b = { fg = "Black", bg = "LightBlue" },
+	c = { fg = "Black", bg = "LightGrey" },
+	y = { fg = "Black", bg = "Pink" },
+	z = { fg = "White", bg = "DeepPink", gui = "bold" },
 }
 
 M.command = {
-	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-	b = { fg = "Black", bg = "Grey" },
-	c = { fg = "Black", bg = "DarkGrey" },
+	a = { fg = "Black", bg = "Cyan", gui = "bold" },
+	b = { fg = "Black", bg = "LightBlue" },
+	c = { fg = "Black", bg = "LightGrey" },
+	y = { fg = "Black", bg = "Pink" },
+	z = { fg = "White", bg = "DeepPink", gui = "bold" },
 }
 
 M.inactive = {
-	a = { fg = "Black", bg = "SteelBlue", gui = "bold" },
-	b = { fg = "Black", bg = "Grey" },
-	c = { fg = "Black", bg = "DarkGrey" },
+	a = { fg = "Black", bg = "Cyan", gui = "bold" },
+	b = { fg = "Black", bg = "LightBlue" },
+	c = { fg = "Black", bg = "LightGrey" },
+	y = { fg = "Black", bg = "Pink" },
+	z = { fg = "White", bg = "DeepPink", gui = "bold" },
 }
 
 return M
