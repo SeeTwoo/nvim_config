8965c1a6ac96e24db7f696b54bb7c1e7533505cb
SeeTwoo <walti.bee@gmail.com>
2025-08-28 11:04:27 +0200

    hello

--- lua/plugin_configs/neotree.lua
+++ lua/plugin_configs/neotree.lua
@@ -1,7 +1,3 @@
 require("neo-tree").setup({
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	}
+	vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {desc = "Toggle Neo-tree" })
 })
