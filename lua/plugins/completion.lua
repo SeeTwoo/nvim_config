57158b5b8073e7509cb73af2262fbcde063cf4e8
SeeTwoo <walti.bee@gmail.com>
2026-01-13 02:32:58 +0100

    yeeees, we have the suggestions just for structs

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,19 +1,37 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies =  {"hrsh7th/cmp-nvim-lsp"},
+	'hrsh7th/nvim-cmp',
+	dependencies = {
+		'hrsh7th/cmp-nvim-lsp', -- Connects cmp to clangd
+		'L3MON4D3/LuaSnip',     -- Snippet engine (required for cmp to work)
+		'saadparwaiz1/cmp_luasnip'
+	},
 	config = function()
-		local cmp = require("cmp")
+		local cmp = require('cmp')
 
 		cmp.setup({
-			completion = {
-				autocomplete = { cmp.TriggerEvent.TextChanged },
+			-- Required: You must specify a snippet engine
+			snippet = {
+				expand = function(args)
+					require('luasnip').lsp_expand(args.body)
+				end,
 			},
-			sources = {
-				{name = "nvim_lsp"}
-			},
+			-- Key mappings
 			mapping = cmp.mapping.preset.insert({
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				['<C-b>'] = cmp.mapping.scroll_docs(-4),
+				['<C-f>'] = cmp.mapping.scroll_docs(4),
+				['<C-Space>'] = cmp.mapping.complete(), -- Manually trigger completion
+				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Enter to confirm
+				['<Tab>'] = cmp.mapping.select_next_item(),
+				['<S-Tab>'] = cmp.mapping.select_prev_item(),
+			}),
+
+			-- VITAL: This restricts what shows up in the menu
+			sources = cmp.config.sources({
+				{
+					name = 'nvim_lsp',
+					keyword_length = 99,
+				}, -- Only shows suggestions from clangd
+				-- { name = 'buffer' }, -- <-- KEEP THIS COMMENTED OUT to avoid random text matches
 			})
 		})
 	end
