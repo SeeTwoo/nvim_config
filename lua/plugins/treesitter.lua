d77ee1464bb2935086127e53260a53f3d2fa8261
SeeTwoo <walti.bee@gmail.com>
2026-01-14 18:34:02 +0100

    removed unnecessary brackets in the treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,18 +1,16 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		branch = "master",
-		build = ":TSUpdate",
-		config = function ()
-			require("nvim-treesitter.configs").setup {
-				ensure_installed = {"c", "lua", "cpp"},
-				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-			}
-		end,
-	}
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	build = ":TSUpdate",
+	config = function ()
+		require("nvim-treesitter.configs").setup {
+			ensure_installed = {"c", "lua", "cpp"},
+			sync_install = false,
+			auto_install = true,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		}
+	end,
 }
